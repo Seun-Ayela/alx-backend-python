0ccbcbd3ef06b6f69d91024f87941e4123ec80fb
iamseun <seunayela@gmail.com>
2023-07-07 03:01:39 +0100

    chmaged 1-concat codes

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
+"""
+python3 -c
+'print(__import__("1-concat").__doc__)'
+Write a type-annotated function to_kv that takes a string k and an int OR float v as arguments and returns a tuple. The first element of the tuple is the string k. The second element is the square of the int/float v and should be annotated as a float.
+
+Author: Oluwaseun Ayela 
+"""
+
 
 def concat(str1: str, str2: str) -> str:
+    """
+    Trying this docstring for this module
+     python3 -c
+     print(__import__("1-concat").concat.__doc__)
+
+    """
     return str1 + str2
+    python3 - c
+    print(__import__("1-concat").concat.__doc__)
