6461d72456ebf934e10d7666cde95ca0cf723222
iamseun <seunayela@gmail.com>
2023-07-07 02:48:32 +0100

    update

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -12,6 +12,6 @@ from typing import Union, Tuple
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     return k, float(v) ** 2
     """
-    print(__import__("7-to_kv").to_kv.__doc__)
     Trying this docstring for this module
     """
+    print(__import__("7-to_kv").to_kv.__doc__)
