8e029814358a5f5d57fa85da90ed91570692daa1
iamseun <seunayela@gmail.com>
2023-07-07 02:37:04 +0100

    new changes to file 7-...

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
+"""
+python3 -c
+'print(__import__("7-to_kv").__doc__)'
+Write a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float.
+
+Author: Oluwaseun Ayela 
+"""
+from typing import Union, Tuple
+
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Trying this docstring for this module
+    """
     return k, float(v) ** 2
+    print(__import__("7-to_kv").to_kv.__doc__)
