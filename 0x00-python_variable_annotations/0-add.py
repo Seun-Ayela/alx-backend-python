e442296e40a9f1c99a6a7784050ce78a7cfd3022
iamseun <seunayela@gmail.com>
2023-07-07 02:45:50 +0100

    made new changes to 0-add file

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
+"""
+python3 -c
+'print(__import__("0-add").__doc__)'
+Write a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float.
+
+Author: Oluwaseun Ayela 
+"""
+
+
 def add(a: float, b: float) -> float:
     return a + b
+    """
+    python3 -c
+    'print(__import__("0-add").add.__doc__)'
+    'print(__import__("0-add").MyClass.my_function.__doc__)'
+    """
