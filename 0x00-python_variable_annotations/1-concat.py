d35af50df900485ff6daa00a5bfe663a937e511d
iamseun <seunayela@gmail.com>
2023-07-08 01:44:43 +0100

    changes the doc of 1- file

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,20 +1,9 @@
 #!/usr/bin/env python3
-"""
-python3 -c
-'print(__import__("1-concat").__doc__)'
-Write a type-annotated function to_kv that takes a string k and an int OR float v as arguments and returns a tuple. The first element of the tuple is the string k. The second element is the square of the int/float v and should be annotated as a float.
 
-Author: Oluwaseun Ayela 
-"""
+
+'''A module that concatenates two strings'''
 
 
 def concat(str1: str, str2: str) -> str:
-    """
-    Trying this docstring for this module
-     python3 -c
-     print(__import__("1-concat").concat.__doc__)
-
-    """
+    '''A function that concatenates 2 strings'''
     return str1 + str2
-    python3 - c
-    print(__import__("1-concat").concat.__doc__)
