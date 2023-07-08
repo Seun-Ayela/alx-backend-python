f4647c9141bdb841be08a2554643772d5c0ff108
iamseun <seunayela@gmail.com>
2023-07-08 01:43:03 +0100

    made new chnages to the file

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,19 +1,8 @@
 #!/usr/bin/env python3
-"""
-python3 -c
-'print(__import__("0-add").__doc__)'
-Write a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float.
 
-Author: Oluwaseun Ayela 
-"""
+'''A python module that returns the summation of two float numbers'''
 
 
 def add(a: float, b: float) -> float:
-    """
-     a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float.
-     python3 -c
-     'print(__import__("0-add).add.__doc__)'
-    """
+    '''A function that returns the summation of two float numbers'''
     return a + b
-    python3 - c
-    'print(__import__("0-add).add.__doc__)'
